4d4bd0fb3a87d4aa01547c22e1958a9cd231e062
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2025-04-22 22:38:44 +0200

    poc: wip

---
+++ .github/actions/some-ts-action/index.ts
@@ -0,0 +1,21 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run(): Promise<void> {
+    try {
+        // Get the input message
+        const message: string = core.getInput('message', { required: true });
+
+        // Write the message to the step summary
+        await core.summary.addRaw(message).write();
+
+        // Log the message to the console
+        core.info(`Message added to step summary: ${message}`);
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+    }
+}
+
+run();
